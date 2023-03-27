cd43dd1e6a15df3b9907009f1c3000569a10425b
lucaspeixotochv <lucaspeixotochaves@live.com>
2023-03-27 11:43:38 -0300

    Enviando arquivos - Exercicios realizado em aula

---
+++ Lista Sequencial/Questao-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("\n Caderno de poesias e um belo lugar.\n Tantas coisas lindas que eu gostaria de falar.\n Eu falo em forma de versos para todos poderem escutar.\n Agora voce ja sabe por que os poetas passam os dias escrevendo em seus\n caderno de poesias.");
+
+    return 0;
+}
---
+++ Lista Sequencial/Questao-6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int numX = 10, numY = 20;
+    int soma = numX + numY;
+    printf("%d + %d = %d", numX, numY, soma);
+
+    return 0;
+}
